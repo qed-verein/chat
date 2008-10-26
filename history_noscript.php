229242294069b581e1fc1abfda55fe9a32733dd9
Christoph-Simon Senjak <webmaster+css@qed-verein.de>
2008-10-26 23:31:29 +0000

    Noscript-Interface mit Autoscrolling.

--- history_noscript.php
+++ history_noscript.php
@@ -1,7 +1,7 @@
 <?php
 
 	$refresh = is_numeric($_GET["refresh"]) ? $_GET["refresh"] : 5;
-	header("Refresh: " . $refresh . "; url=" . $_SERVER['REQUEST_URI']);
+	header("Refresh: " . $refresh . "; url=" . $_SERVER['REQUEST_URI'] . "#bottom");
 	
 	require("history.php");
 
