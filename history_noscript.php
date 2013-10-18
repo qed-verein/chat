ab97c9bf5a238c82a89f445a69b0a0555c6f6da4
Christoph-Simon Senjak <webmaster+css@qed-verein.de>
2013-10-18 20:26:38 +0000

    schreibfehler

--- history_noscript.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$refresh = is_numeric($_GET["refresh"]) ? $_GET["refresh"] : 5;
-	header("Refresh: " . $refresh . "; url=" . $_SERVER['REQUEST_URI'] . "#bottom");
-	
-	require("history.php");
-
-?>
