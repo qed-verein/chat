eec4e2e64b95908ed28ed4d27b6593b3b93a0d0a
Christoph-Simon Senjak <webmaster+css@qed-verein.de>
2013-10-18 18:37:50 +0000

    test again

--- send2.js
+++ send2.js
@@ -113,8 +113,9 @@ function Send ()
 				request.setRequestHeader ("Content-Type", "application/x-www-form-urlencoded");
 				request.setRequestHeader ("Content-Encoding", "utf-8");
 				//%%user \neq bot
+				alert(options["channel"]);
 				var content = 
-					"delay=" + position + "&channel=test" + /* options["channel"] +*/ "&name=" + encodeURIComponent (document.getElementById ("name").value) + "&message=" + encodeURIComponent (document.getElementById ("message").value)+"&bottag="+zero;
+					"delay=" + position + "&channel=" + options["channel"] + "&name=" + encodeURIComponent (document.getElementById ("name").value) + "&message=" + encodeURIComponent (document.getElementById ("message").value)+"&bottag="+zero;
 				if (generator)
 					content += "&key=" + GetKey (generator++);
 				request.send (content);
