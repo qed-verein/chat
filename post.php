525b85bf22cfa6d30135a848f1940a641a4a96c3
Christoph-Simon Senjak <webmaster+css@qed-verein.de>
2013-10-18 16:53:44 +0000

    So jetzt

--- post.php
+++ post.php
@@ -35,12 +35,12 @@
 
 		/* TODO: Little Bobby Tables laesst gruessen ... - CSS */
 
-		$sql = 'INSERT INTO ' . SQL_TABLE . ' (date, delay, ip, name, message, user_id, bottag, channel) VALUES ("' . $post["date"]
+		/*$sql = 'INSERT INTO ' . SQL_TABLE . ' (date, delay, ip, name, message, user_id, bottag, channel) VALUES ("' . $post["date"]
 			. '", ' . $post["delay"] . ', "' . $post["ip"] . '", "' . escape_string($post["name"]) . '", "' 
 			. escape_string ($post["message"])
-			. '", ' . intval($post['userid']) .','.$bottag.', "'.$post["channel"].'")';
+			. '", ' . intval($post['userid']) .','.$bottag.', "'.$post["channel"].'")';*/
 
-		/*$sql = sprintf('INSER INTO %s (date, delay, ip, name, message, user_id, bottag, channel) VALUES ("%s", %d, "%s", "%s", "%s", %d, %d, "%s")',
+		$sql = sprintf('INSERT INTO %s (date, delay, ip, name, message, user_id, bottag, channel) VALUES ("%s", %d, "%s", "%s", "%s", %d, %d, "%s")',
 					SQL_TABLE,
 			       mysql_real_escape_string($post["date"]),
 			       $post["delay"],
@@ -49,7 +49,7 @@
 				   mysql_real_escape_string($post["message"]),
 				   intval($post["userid"]),
 				   $bottag,
-				   mysql_real_escape_string($post["channel"]));*/
+				   mysql_real_escape_string($post["channel"]));
 
 		mysql_query ($sql);
 		mysql_close();
