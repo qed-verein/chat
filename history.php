2c4406c85b53d7ef9d71c99c4e90070baac6b6cc
Manfred Paul <webmaster+manfred.paul@qed-verein.de>
2016-01-17 21:16:32 +0000

    Caching unterdrückt in history.php

--- history.php
+++ history.php
@@ -16,6 +16,8 @@ set_exception_handler('ExceptionHandler');
 versionCheck();
 
 header('Content-Type: text/plain; charset=utf-8');
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 
 $channel = uriParamString('channel', '');
 $last = uriParamInteger('last', 100);
