86ed6610fd98c72250df53a0f99c12c71e390089
Tamás Korodi <webmaster+tamas.korodi@qed-verein.de>
2016-10-22 18:55:10 +0000

    Testversion für Ruby

---
+++ index_ruby.php
@@ -0,0 +1,33 @@
+<?php
+$ignore_no_login = true;
+
+require_once('common.php');
+
+if(!userLoggedIn())
+	redirect(urlLogin(chatOptions()));
+
+$layout = uriParamString('layout', 'screen');
+$frame = uriParamString('frame', '');
+
+if($layout == 'mobile')
+{
+	$parts = array('mobile', 'receiver', 'send_mobile', 'settings', 'logs');
+	require('template_ruby.php');
+}
+elseif($layout == 'screen')
+{
+	$parts = array('screen', 'receiver', 'send', 'settings', 'logs');
+	require('template_ruby.php');
+}
+elseif($layout == 'frames')
+{
+	if(in_array($frame, array('receiver', 'send', 'settings', 'logs')))
+	{
+		$parts = array('frames', $frame);
+		require('template_ruby.php');
+	}
+	else require('frames.html');
+}
+else die("Unbekanntes Layout");
+
+?>
