9cdb542ae523a393b4582550bcd81681bd1c2105
Tamás Korodi <webmaster+tamas.korodi@qed-verein.de>
2016-10-24 22:35:45 +0000

    Rubyversion vom QED-Chat befindet sich jetzt in /neu

--- index_ruby.php
+++ index_ruby.php
@@ -1,33 +1,5 @@
 <?php
-$ignore_no_login = true;
 
-require_once('common.php');
-
-if(!userLoggedIn())
-	redirect(urlLogin(chatOptions()));
-
-$layout = uriParamString('layout', 'screen');
-$frame = uriParamString('frame', '');
-
-if($layout == 'mobile')
-{
-	$parts = array('mobile', 'receiver', 'send_mobile', 'settings', 'logs');
-	require('template_ruby.php');
-}
-elseif($layout == 'screen')
-{
-	$parts = array('screen', 'receiver', 'send', 'settings', 'logs');
-	require('template_ruby.php');
-}
-elseif($layout == 'frames')
-{
-	if(in_array($frame, array('receiver', 'send', 'settings', 'logs')))
-	{
-		$parts = array('frames', $frame);
-		require('template_ruby.php');
-	}
-	else require('frames.html');
-}
-else die("Unbekanntes Layout");
+die('Die neue Version des QED-Chats befindet sich unter https://chat.qed-verein.de:31416/neu/chat.html');
 
 ?>
