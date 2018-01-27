af01e6a906ba7c58b43fabb004f7dc26f7bd3593
Christoph-Simon Senjak <you@example.com>
2018-01-27 12:51:59 +0100

    AGPL angewandt

--- rubychat-config.example.rb
+++ rubychat-config.example.rb
@@ -1,4 +1,22 @@
-# Geänderte Konfiguration in rubychat-config.rb speichern
+# Copyright (C) 2004-2018 Quod Erat Demonstrandum e.V. <webmaster@qed-verein.de>
+#
+# This file is part of QED-Chat.
+#
+# QED-Chat is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# QED-Chat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with QED-Chat.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+# Geaenderte Konfiguration in rubychat-config.rb speichern
 
 $sqlConfig = {:adapter => "mysql2", :host => "localhost",
 	:database => "Datenbankname", :username => "Benutzername", :password => "Passwort"}
@@ -9,6 +27,6 @@ $wsPort = 21000
 $wsPingInterval = 60
 $wsFailsToTimeout = 3
 
-$hostname = "chat.foo.bar" #Muss geändert werden, z.B. localhost für lokales Testen
+$hostname = "chat.foo.bar" #Muss geaendert werden, z.B. localhost fuer lokales Testen
 
 $secureCookies = true #False, falls lokal entwickelt wird (ohne https)
